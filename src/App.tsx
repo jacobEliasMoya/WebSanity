d9495e277b873cdc5fd8fa517ec8c88bbcfbffa9
Jake <jemoya1994@gmail.com>
2025-03-03 21:54:03 -0800

    fixes to push through netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react"
+// import { useState } from "react"
 import Header from "./components/Layout/Header"
 import Section from "./components/Layout/Sections/Section"
 
 function App() {
 
-  const [isDark,setIsDark] = useState<boolean>(false)
+  // const [isDark,setIsDark] = useState<boolean>(false)
 
-  const handleDarkMode = () => {
-    setIsDark(prev=> !prev)
-  }
+  // const handleDarkMode = () => {
+  //   setIsDark(prev=> !prev)
+  // }
 
   return (
     <main className="bg-white min-h-dvh">
