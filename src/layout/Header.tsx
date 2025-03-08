a3881de96feee82d5504fd9dc5c57953a7b88f41
jacob moya <jemoya1994@Gmail.com>
2025-03-08 01:18:54 -0800

    small updates for the header height on load

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -52,14 +52,14 @@ const Header = () => {
   },[])
 
   return (
-    <header className=" sticky top-0 w-full mx-auto bg-neutral-950/00  backdrop-blur-lg border-2 border-red-500 filter text-indigo-950 font-dm-serif text-lg grid grid-cols-1 ">
+    <header className=" sticky top-0 w-full mx-auto bg-neutral-950/00  backdrop-blur-lg border-2 border-red-500 filter text-indigo-950 font-dm-serif text-lg grid grid-cols-1 h-30">
       <div className="grid grid-cols-3 gap-8 ">
        
         <ul className={`hidden md:grid grid-cols-4 h-full`}>
           { navigationElms?.map((item)=>(
             <li key={`nav-${item.id}`} className="first:text-black first:bg-red-500 text-white first:font-normal relative after:absolute after:bottom-0 after:h-2  after:left-0 after:w-full hover:after:h-full after:transition-all after:-z-10 z-10 flex flex-row gap-2 items-center justify-center border-2 border-y-0 border-red-500 border-r-0 first:border-l-0 last:border-r-2 hover:bg-red-500 hover:text-neutral-950">
               <a aria-label={item.title} href={item.url} target={item.target ? item.target : '' } className="w-full h-full flex items-center justify-center"> 
-                <span className="hidden lg:block ">{item.title}</span> 
+                <span className="text-sm md:text-lg hidden lg:block ">{item.title}</span> 
                 <span className={`hidden md:block lg:hidden ${item.additionalClasses}`}>{item.icon}</span> 
               </a>
             </li>
