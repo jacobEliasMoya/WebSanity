6e21f8beb758f760b2e537d1fe27099b03bb3f02
Jake <jemoya1994@gmail.com>
2025-03-18 23:59:11 -0700

    fetch looked funky .... swapped to axios :eyes

--- src/layout/RandomQuote.tsx
+++ src/layout/RandomQuote.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { QuoteInterface } from "../types/QuoteApiInterface";
+import axios from "axios";
 
 
 const RandomQuote = () => {
@@ -7,7 +8,14 @@ const RandomQuote = () => {
     const [quote,setQuote] = useState<QuoteInterface >()
 
     useEffect(()=>{
-        fetch('https://api.quotable.io/random').then(data=>data.json()).then(data=>setQuote(data))
+        axios.get('https://api.quotable.io/random')
+        .then(response=>{
+            setQuote(response.data)
+        })
+        .catch(err=>{
+            console.error(err)
+        })
+
     },[])
 
     useEffect(()=>{
@@ -16,13 +24,13 @@ const RandomQuote = () => {
 
 
   return (
-        <section className='bg-cover bg-center w-full min-h-15 fixed bottom-0 bg-zinc-300/70 backdrop-blur-md border-t-2 flex-row flex items-center justify-start py-2 px-4'>
-            <div className="w-4/6 flex-row flex items-center justify-start gap-10">
+        <section className='bg-cover bg-center w-full min-h-15 fixed bottom-0 bg-zinc-300/70 backdrop-blur-md border-t-2 flex-row flex items-center justify-between py-2 px-4'>
+            <div className=" flex-row flex items-center justify-start gap-10">
                 <h2 className="font-dm-serif text-xl  text-center " >Quote of the Day</h2>
                 <h3 className="font-inter text-md  text-center " >"{quote ? quote.content.split('.')[0] + "." : 'looking for your quote  '}"</h3>
             </div>
 
-            <div className="w-2/6 flex-row flex items-center justify-end gap-10">
+            <div className=" flex-row flex items-center justify-end gap-10">
                 <h2 className="font-dm-serif text-2xl  text-center " >{quote ? quote.author : 'looking for your author  '}</h2>
             </div>
         </section>  
